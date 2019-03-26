990a2eb79a1a30ea3793508269a2df8e9b284ea6
aparcerozas <aparcerozas@victoria11.danielcastelao.org>
2019-03-26 10:11:45 +0100

    TwitterMenu

--- AppTwitter/src/apptwitter/AppTwitter.java
+++ AppTwitter/src/apptwitter/AppTwitter.java
@@ -6,6 +6,7 @@
 package apptwitter;
 
 import java.util.List;
+import javax.swing.JOptionPane;
 import twitter4j.*;
 
 /**
@@ -20,24 +21,36 @@ public class AppTwitter {
      */
     public static void main(String[] args) throws TwitterException {
         // TODO code application logic here
-        //Realizar un tuit
+        int opcion;
         Twitter twitter = TwitterFactory.getSingleton();
-        Status st = twitter.updateStatus("Ocupado");
-        System.out.println("Successfully updated the status to [" + st.getText() + "].");
+        do{
+            opcion = Integer.parseInt(JOptionPane.showInputDialog("App Twitter"
+                    + "\n1. Realizar un tuit\n2. Mostrar la línea del tiempo"
+                    + "\n3. Filtrar\n4. Salir"));
+            switch(opcion){
+                //Realizar un tuit
+                case 1: String tuit = JOptionPane.showInputDialog("Introduzca un tuit:");
+                Status st = twitter.updateStatus(tuit);
+                JOptionPane.showMessageDialog(null, "Realizado correctamente el tuit [" + st.getText() + "].");
+                break;
+                //Mostrar la línea de tiempo
+                case 2: List<Status> statuses = twitter.getHomeTimeline();
+                System.out.println("Mostrando la línea de tiempo:");
+                for (Status status : statuses) {
+                    System.out.println(status.getUser().getName() + ":" + status.getText());
+                }
+                break;
+                //Filtrado
+                case 3: Query query = new Query("source:twitter4j yusukey");
+                QueryResult result = twitter.search(query);
+                for (Status status : result.getTweets()) {
+                    System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+                }
+                break;
+                case 4: System.exit(0);
+            }
+        }while(opcion!=4);
         
-        //Traer la línea de tiempo
-        List<Status> statuses = twitter.getHomeTimeline();
-        System.out.println("Showing home timeline.");
-        for (Status status : statuses) {
-            System.out.println(status.getUser().getName() + ":" + status.getText());
-        }
-        
-        //Filtrado
-        Query query = new Query("source:twitter4j yusukey");
-        QueryResult result = twitter.search(query);
-        for (Status status : result.getTweets()) {
-            System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
-        }
     }
     
 }
